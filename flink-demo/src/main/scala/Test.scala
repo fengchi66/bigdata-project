ff22001c6b0dca62440bca9bff88c97090fcd459
wufengchi <wufengchi@yit.com>
2021-03-04 21:53:06 +0800

    init

--- flink-demo/src/main/scala/Test.scala
+++ flink-demo/src/main/scala/Test.scala
@@ -2,9 +2,13 @@ import org.apache.flink.streaming.api.scala._
 
 object Test {
   def main(args: Array[String]): Unit = {
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-//    env.addSource(new Source).assignAscendingTimestamps(_.ts)
+    val env = StreamExecutionEnvironment. getExecutionEnvironment
+
+    env.fromCollection(List(1,2,3,4,5,6,7,8)).print()
+
+    env.execute("Job")
+
   }
 
 }
