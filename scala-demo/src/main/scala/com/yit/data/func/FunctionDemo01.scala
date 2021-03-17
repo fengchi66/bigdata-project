de90dba814fdeeb298c82e7a1ce4c0ed59cd136a
wufengchi <wufengchi@yit.com>
2021-03-17 15:42:11 +0800

    init

--- scala-demo/src/main/scala/com/yit/data/func/FunctionDemo01.scala
+++ scala-demo/src/main/scala/com/yit/data/func/FunctionDemo01.scala
@@ -4,19 +4,27 @@ object FunctionDemo01 extends App {
 
   val aa = "AbC"
 
+  /**
+   * 函数值
+   */
   val f1 = (a: String) => a.toLowerCase
 
   val f2 = new Function[String, String] {
     override def apply(v1: String): String = v1.toLowerCase
   }
 
-  val f3 = new (String, String) {
+  val f3 = new (String => String) {
     def apply(s: String): String = s.toLowerCase
-
-
-    println(f1(aa))
-
-
   }
+
+  val lower: String => String = _.toLowerCase
+
+  val add: (Int, Int) => Int = _ + _
+  // 或者
+  def add(a: Int, b: Int): Int = a + b
+
+
+  println(lower("sdKiFG"))
+  println(add(1, 2))
 }
 
