2e71501681c7fdef00ba744b3598f9dd6a9a0547
邬凤池 <wufengchi@wufengchideMacBook-Pro.local>
2021-03-17 16:22:22 +0800

    init

--- scala-demo/src/main/scala/com/yit/data/func/FunctionDemo01.scala
+++ scala-demo/src/main/scala/com/yit/data/func/FunctionDemo01.scala
@@ -19,9 +19,18 @@ object FunctionDemo01 extends App {
 
   val lower: String => String = _.toLowerCase
 
-  val add: (Int, Int) => Int = _ + _
+//  val add: (Int, Int) => Int = _ + _
   // 或者
   def add(a: Int, b: Int): Int = a + b
+
+  val salaries = Seq(20000, 70000, 40000)
+  val newSalaries = salaries.map(x => x * 2) // List(40000, 140000, 80000)
+
+  def f4(f: (Int, Int) => Int): Int = {
+    f(2, 4)
+  }
+
+ println(f4(add))
 
 
   println(lower("sdKiFG"))
